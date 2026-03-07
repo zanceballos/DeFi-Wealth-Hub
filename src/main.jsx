3e63180615b0b86abfe221c5405d6401770a25e6
kang <lowkangxuan@gmail.com>
2026-03-07 13:59:32 +0800

    Push

--- src/main.jsx
+++ src/main.jsx
@@ -4,28 +4,34 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './index.css'
 import App, {AdvisoryPage, NotFoundPage, PrivacyPage} from './App.jsx'
 import Dashboard from './pages/Dashboard.jsx'
-import Home from "./pages/Home.jsx";
-import Portfolio from "./pages/Portfolio.jsx";
-import Transactions from "./pages/Transactions.jsx";
-import {AppProvider} from "./context/AppContext.jsx";
-
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <AppProvider>
-            <BrowserRouter>
+        <BrowserRouter>
+            <AuthProvider>
                 <Routes>
-                    <Route path="/" element={<App/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path="portfolio" element={<Portfolio/>}/>
-                        <Route path="transactions" element={<Transactions/>}/>
-                        <Route path="dashboard" element={<Dashboard/>}/>
-                        <Route path="advisory" element={<AdvisoryPage/>}/>
-                        <Route path="privacy" element={<PrivacyPage/>}/>
-                        <Route path="*" element={<NotFoundPage/>}/>
+                    {/* Public routes */}
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignUpPage/>}/>
+
+                    {/* Protected routes */}
+                    <Route element={<ProtectedRoute/>}>
+                        <Route path="/onboarding" element={<OnboardingPage/>}/>
+                        <Route path="/" element={<App/>}>
+                            <Route index element={<Dashboard/>}/>
+                            <Route path="dashboard" element={<Dashboard/>}/>
+                            <Route path="advisory" element={<AdvisoryPage/>}/>
+                            <Route path="privacy" element={<PrivacyPage/>}/>
+                            <Route path="" element={<NotFoundPage/>}/>
+                        </Route>
                     </Route>
                 </Routes>
-            </BrowserRouter>
-        </AppProvider>
+            </AuthProvider>
+        </BrowserRouter>
     </StrictMode>,
 )
